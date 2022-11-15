1e3b8103f2f7cbb8f381eee01bf22bed99f40036
JB Krisna Arianta <93109103+krisnaarianta@users.noreply.github.com>
2022-11-15 19:23:16 +0700

    feat: Kritik & Saran

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -1,10 +1,12 @@
-
+import Kritik from "../components/HomePage/kritikSaran";
 function Beranda() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
       Hello Beranda!
     </h1>
+
+    <Kritik/>
     </>
   );
 }

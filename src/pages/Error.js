9e8aa31f4b664c631e4e5cdaf30b9c1da8bb8569
salmaasmanadia <salma.asma.nadia@mail.ugm.ac.id>
2022-11-16 21:24:03 +0700

    feat: Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,8 +3,12 @@ function Error() {
   return (
     <>
       <div class="bg-[#E1DAD1] h-full w-full">
-        <div class="sm:bg-contain  sm:w-[1440px] sm:h-[770px] lg:md:h-full lg:md:w-full bg-no-repeat md:bg-contain" style={{ backgroundImage: "url(" + bgError + ")" }}>
-          <h1 class="text-[#2A374A] font-bold font-Playfair text-[45px] text-center sm:p-[90px] lg:md:p-[270px]">ERROR 404</h1>
+        <div
+          class="lg:bg-cover sm:bg-contain
+         w-full lg:h-auto sm:h-auto sm:bg-no-repeat lg:bg-repeat-x"
+          style={{ backgroundImage: "url(" + bgError + ")" }}
+        >
+          <h1 class="text-[#2A374A] font-bold font-Playfair lg:text-[120px] sm:text-[45px] text-center sm:p-[400px] lg:p-[295px]">ERROR 404</h1>
         </div>
       </div>
     </>

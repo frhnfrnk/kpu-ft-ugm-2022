d27837dd36b9382a25859672a6684f60aa39e6a5
salmaasmanadia <salma.asma.nadia@mail.ugm.ac.id>
2022-11-16 20:42:58 +0700

    feat: edit tentangKPU

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,7 +4,7 @@ function Error() {
     <>
       <div class="bg-[#E1DAD1] h-full w-full">
         <div class="sm:bg-contain  sm:w-[1440px] sm:h-[770px] lg:md:h-full lg:md:w-full bg-no-repeat md:bg-contain" style={{ backgroundImage: "url(" + bgError + ")" }}>
-          <h1 class="text-[#2A374A] font-bold font-playfair text-[45px] text-center sm:p-[90px] lg:md:p-[270px]">ERROR 404</h1>
+          <h1 class="text-[#2A374A] font-bold font-Playfair text-[45px] text-center sm:p-[90px] lg:md:p-[270px]">ERROR 404</h1>
         </div>
       </div>
     </>
--- src/pages/Tentang.js
+++ src/pages/Tentang.js
@@ -5,57 +5,33 @@ export default function Tentang() {
   return (
     <>
       {/* bagian tentang kami */}
-      <div className="bg-[#E1DAD1] ">
-        <div className="bg-cover[length:full_300px]  p-[20px] bg-no-repeat" style={{ backgroundImage: "url(" + Backbaru + ")" }}>
-          <h1 className="pt-[40px]  text-center text-[28px] sm:text-[12px] font-playfair text-[#2A374A] ">Tentang Kami</h1>
-          <img src="/Logo_KPU.jpg" className="m-auto w-[148px] h-[148px]" alt="logokpu" />
+      <div className="w-auto h-auto bg-cover bg-[#E1DAD1]  ">
+        <div className="bg-cover bg-no-repeat w-full h-[100vh] p-[20px] " style={{ backgroundImage: "url(" + Backbaru + ")" }}>
+          <h1 className="pt-[10vh] m-[4px] text-center sm:text-[28px] lg:text-[50px] font-Playfair text-[#2A374A] font-bold">Tentang Kami</h1>
+          <img src="/Logo_KPU.jpg" className="m-auto lg:w-[240px] lg:h-[240px] sm:w-[148px] sm:h-[148px]" alt="logokpu" />
           <div>
-            <h2 className="text-center mt-4 font-lato text-[18px] text-[#2A374A]">Komisi Pemilihan Umum</h2>
-            <h2 className="text-center font-lato text-[18px] text-[#2A374A] mb-8 ">Fakultas Teknik UGM 2022</h2>
+            <h2 className="text-center mt-4 font-Lato sm:text-[18px] lg:text-[30px] font-medium text-[#2A374A]">Komisi Pemilihan Umum</h2>
+            <h2 className="text-center font-Lato sm:text-[18px] lg:text-[30px] font-medium text-[#2A374A] mb-8 ">Fakultas Teknik UGM 2022</h2>
           </div>
         </div>
         <div className="m-4">
-          <h1 className="text-center text-[18px]  text-bold font-playfair text-[#2A374A] mt-20">PEMILU FAKULTAS TEKNIK UGM</h1>
-          <p className="text-center text-[12px] font-lato m-2 p-2 text-[#35455D]">
-            Pemilihan Umum Mahasiswa Fakultas Teknik Universitas Gadjah Mada (Pemilu) adalah sarana pelaksanaan kedaulatan mahasiswa berdasarkan Anggaran Dasar/Anggaran Rumah Tangga Keluarga Mahasiswa Fakultas Teknik Universitas Gadjah //
+          <h1 className="text-center sm:text-[18px] lg:text-[30px] font-bold font-Playfair text-[#2A374A] mt-20">PEMILU FAKULTAS TEKNIK UGM</h1>
+          <p className="text-center sm:text-[12px] lg:text-[18px] font-medium font-Lato m-2 p-2 text-[#35455D] ">
+            Pemilihan Umum Mahasiswa Fakultas Teknik Universitas Gadjah Mada (Pemilu) adalah sarana pelaksanaan kedaulatan mahasiswa berdasarkan Anggaran Dasar/Anggaran Rumah Tangga Keluarga Mahasiswa Fakultas Teknik Universitas Gadjah
             Mada. <br />
             <br />
             Peserta Pemilu adalah kandidat calon Ketua BEM KMFT UGM yang telah lolos verifikasi oleh KPU.
             <br />
             <br />
-            Pemilih adalah seluruh mahasiswa Fakultas Teknik Universitas Gadjah Mada yang terdaftar secara akademik pada jenjang S1 // reguler yang mempunyai hak untuk memilih dalam Pemilu.
+            Pemilih adalah seluruh mahasiswa Fakultas Teknik Universitas Gadjah Mada yang terdaftar secara akademik pada jenjang S1 reguler yang mempunyai hak untuk memilih dalam Pemilu.
           </p>
-          <h1 className="text-[18px]  text-bold text-center font-playfair text-[#2A374A]">KPU FAKULTAS TEKNIK UGM</h1>
-          <p className="text-center text-[12px] text-[#35455D] font-lato  p-2 mb-20">
+          <h1 className="sm:text-[18px] lg:text-[30px]  font-bold text-center font-Playfair text-[#2A374A]">KPU FAKULTAS TEKNIK UGM</h1>
+          <p className="text-center sm:text-[12px] lg:text-[18px] font-medium text-[#35455D] font-Lato  p-5 mb-20">
             Komisi Pemilihan Umum Fakultas Teknik Universitas Gadjah Mada yang selanjutnya disebut KPU adalah suatu komisi yang menyelenggarakan Pemilu berdasarkan Undang-Undang KMFT UGM tentang Pemilu dan terdiri dari SC KPU, OC KPU,
             Panitia Pengawas Pemilu dan Mahkamah Pemilu.
           </p>
-          <div className="bg-cover pt-px bg-no-repeat " style={{ backgroundImage: "url(" + Backbaru2 + ")" }}>
-            <h1 className="text-center text-[18px]  text-bold font-playfair text-[#2A374A] mt-20">Panitia KPU FT UGM 2022</h1>
-            <container>
-              <img src="" alt="" />
-              <h2>Staf Acara</h2>
-              <p>a</p>
-              <p>b</p>
-              <p>c</p>
-              <p></p>
-            </container>
-            <container>
-              <img src="" alt="" />
-              <h2>Staf Acara</h2>
-              <p></p>
-              <p></p>
-              <p></p>
-              <p></p>
-            </container>
-            <container>
-              <img src="" alt="" />
-              <h2>Staf Acara</h2>
-              <p></p>
-              <p></p>
-              <p></p>
-              <p></p>
-            </container>
+          <div className="bg-cover w-full bg-repeat-x  " style={{ backgroundImage: "url(" + Backbaru2 + ")" }}>
+            <h1 className="text-center sm:text-[18px] lg:text-[30px] font-Playfair font-bold text-[#2A374A] mt-30">Panitia KPU FT UGM 2022</h1>
           </div>
         </div>
       </div>

bb119c3689c27742cd4fd9235c44cf883fbf1149
Ahmad Fauzan <ahmadfauzan190603@gmail.com>
2022-11-20 22:22:08 +0700

    fix conflict again

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -1,9 +1,10 @@
 import Countdown from "../components/Beranda/Countdown";
+import Hero from "../components/Beranda/Hero";
 import Kritik from "../components/HomePage/kritikSaran";
 function Beranda() {
   return (
     <>
-
+    <Hero/>
     <Countdown />
     <Kritik />
     </>
